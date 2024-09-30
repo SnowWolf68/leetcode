7def50f09b70d41169b26248be6f6648ee8d4f0e
SnowWolf68 <zhangjiashuo0818@163.com>
2024-09-30 11:39:36 +0800

    update

--- problemList/graph/graph.java
+++ problemList/graph/graph.java
@@ -1,4 +1,5 @@
 // 图论
 //      1. Dijkstra
 //          板子题: LC743 https://leetcode.cn/problems/network-delay-time/description/
-//              
+//          板子题稍微改动一下: LC3112 https://leetcode.cn/problems/minimum-time-to-visit-disappearing-nodes/description/   需要理解dijkstra的本质, 挺有意思的
+//          
