ffaa722b2f6db7fd77066a28a563e546d1227ca8
SnowWolf68 <zhangjiashuo0818@163.com>
2024-10-02 17:50:22 +0800

    update

--- problemList/string/string.java
+++ problemList/string/string.java
@@ -1,3 +1,5 @@
 //  字符串
 //      前后缀分解
-//                  
+//                LC2565 https://leetcode.cn/problems/subsequence-with-the-minimum-score/description/
+//                LC3302 https://leetcode.cn/problems/find-the-lexicographically-smallest-valid-sequence/description/
+//          
