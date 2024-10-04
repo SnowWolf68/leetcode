f7197d6d04b530365cc4dbd0cda63e31f1d6c8f0
SnowWolf68 <zhangjiashuo0818@163.com>
2024-10-04 22:24:09 +0800

    update

--- problemList/slidingWindow/slidingWindows.java
+++ problemList/slidingWindow/slidingWindows.java
@@ -21,4 +21,5 @@
 //          LC1004 https://leetcode.cn/problems/max-consecutive-ones-iii/description/
 //          LC1658 比较显然的转化 https://leetcode.cn/problems/minimum-operations-to-reduce-x-to-zero/description/
 //          LC1838 这题竟然也能用滑窗, 巧妙至极 https://leetcode.cn/problems/frequency-of-the-most-frequent-element/description/
+//          LC2516 https://leetcode.cn/problems/take-k-of-each-character-from-left-and-right/description/
 //          
