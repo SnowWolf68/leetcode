d942f5c497171a82ec55486f51135fcbfb76f261
SnowWolf68 <zhangjiashuo0818@163.com>
2024-10-05 22:45:47 +0800

    update

--- problemList/bisect/bisect.java
+++ problemList/bisect/bisect.java
@@ -2,3 +2,7 @@
 // 
 // 二分求最小
 //      LC3296 https://leetcode.cn/problems/minimum-number-of-seconds-to-make-mountain-height-zero/description/
+
+
+// unsorted
+//      LC2187 每日一题 https://leetcode.cn/problems/minimum-time-to-complete-trips/description/?envType=daily-question&envId=2024-10-05
