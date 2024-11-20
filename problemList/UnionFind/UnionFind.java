f0aebfec49622d841edaf296771767c515fb1ab3
SnowWolf68 <zhangjiashuo0818@163.com>
2024-11-20 13:25:28 +0800

    update

--- problemList/UnionFind/UnionFind.java
+++ problemList/UnionFind/UnionFind.java
@@ -3,3 +3,7 @@
 //      LC684 https://leetcode.cn/problems/redundant-connection/description/?envType=daily-question&envId=2024-10-28    解法不难, 但是并不是很容易想到
 //      LC685 https://leetcode.cn/problems/redundant-connection-ii/?envType=daily-question&envId=2024-10-28     有点难, 需要考虑清楚会出现什么情况
 //      
+//
+//   区间并查集: 
+//      LC3244 https://leetcode.cn/problems/shortest-distance-after-road-addition-queries-ii/description/   也有不用并查集的做法
+//      
--- problemList/unsorted/LC3244_2.java
+++
@@ -1,7 +0,0 @@
-package problemList.unsorted;
-
-public class LC3244_2 {
-    public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
-        
-    }
-}
