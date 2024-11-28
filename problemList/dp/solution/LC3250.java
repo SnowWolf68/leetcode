c76037e504381804cdce899c3a45c719a8ed8328
SnowWolf68 <zhangjiashuo0818@163.com>
2024-11-28 11:54:47 +0800

    update

---
+++ problemList/dp/solution/LC3250.java
@@ -0,0 +1,27 @@
+package problemList.dp.solution;
+
+/**
+arr1[i] + arr2[i] == nums[i]
+-> arr2[i] = nums[i] - arr1[i]
+枚举arr1[i], arr2[i]就可以确定下来
+从前往后一起填写arr1, arr2, 对于每一个下标i, 此时要填的arr1[i]需要满足:
+arr1[i] >= arr1[i - 1]
+arr2[i] = nums[i] - arr1[i] <= arr2[i - 1]  --> arr1[i] >= nums[i] - arr2[i - 1]
+
+因此在dp的过程中, 还需要记录 上一个位置的arr1[i - 1], arr2[i - 1] 的值
+
+dp[i][j][k]: 表示考虑 [0, i] 区间的下标, 并且arr1[i - 1] = j, arr2[i - 1] = k, 此时单调数组对的数目
+dp[i][j][k]: 
+    首先枚举下标i
+        然后枚举arr1[i]的所有可能: 
+            这里arr1[i]的范围: [Math.max(arr1[i - 1], nums[i] - arr2[i - 1]), nums[i]]
+            对于每一个可能的arr1[i], 这里假设枚举到的值为 p , 那么
+            首先能够计算出来此时 arr2[i] = nums[i] - p
+            dp[i][p][]
+
+ */
+public class LC3250 {
+    public int countOfPairs(int[] nums) {
+        
+    }
+}
