686d1b15fa28edacc0f2f26f76d479841e9d41e6
SnowWolf68 <zhangjiashuo0818@163.com>
2024-12-05 22:04:45 +0800

    update

--- problemList/misc/solution/LC2056.java
+++ problemList/misc/solution/LC2056.java
@@ -39,9 +39,10 @@ public class LC2056 {
     private void dfs(int i, List<int[]> path){
         if(i == n){
             cnt++;
-            // for(int[] pa : path){
-            //     System.out.println(Arrays.toString(pa));
-            // }
+            for(int[] pa : path){
+                System.out.print(Arrays.toString(pa) + " ");
+            }
+            System.out.println();
             return;
         }
         int[][] dir = dirs.get(pieces[i]);
@@ -62,31 +63,155 @@ public class LC2056 {
     }
 
     private boolean check(int idx, List<int[]> path){
-        for(int i = 0;i <= idx;i++){
-            for(int j = i + 1;j <= idx;j++){
-                if(!checkTwo(i, j, path.get(i), path.get(j))) return false;
+        if(idx == 0){
+            int[] move = path.get(0);
+            int nx = positions[0][0] - 1 + move[0] * move[2], ny = positions[0][1] - 1 + move[1] * move[2];
+            if(nx < 0 || nx >= 8 || ny < 0 || ny >= 8) return false;
+            else return true;
+        }else{
+            for(int i = 0;i <= idx;i++){
+                for(int j = i + 1;j <= idx;j++){
+                    if(!checkTwo(i, j, path.get(i), path.get(j))) return false;
+                }
             }
+            return true;
+        }
+    }
+
+    private boolean checkTwo2(int i, int j, int[] move1, int[] move2){
+        int x1 = positions[i][0] - 1, y1 = positions[i][1] - 1;
+        int x2 = positions[j][0] - 1, y2 = positions[j][1] - 1;
+        
+        // 检查初始位置
+        if((x1 == x2 && y1 == y2)) return false;
+        
+        // 获取最终位置
+        int ex1 = x1 + move1[0] * move1[2];
+        int ey1 = y1 + move1[1] * move1[2];
+        int ex2 = x2 + move2[0] * move2[2];
+        int ey2 = y2 + move2[1] * move2[2];
+        
+        // 检查边界
+        if(ex1 < 0 || ex1 >= 8 || ey1 < 0 || ey1 >= 8 || 
+           ex2 < 0 || ex2 >= 8 || ey2 < 0 || ey2 >= 8) {
+            return false;
+        }
+        
+        // 检查最终位置是否相同
+        if(ex1 == ex2 && ey1 == ey2) return false;
+        
+        // 检查移动过程中是否相交
+        for(int step = 1; step <= Math.max(move1[2], move2[2]); step++) {
+            int nx1 = x1, ny1 = y1, nx2 = x2, ny2 = y2;
+            
+            if(step <= move1[2]) {
+                nx1 = x1 + move1[0] * step;
+                ny1 = y1 + move1[1] * step;
+            } else {
+                nx1 = ex1;
+                ny1 = ey1;
+            }
+            
+            if(step <= move2[2]) {
+                nx2 = x2 + move2[0] * step;
+                ny2 = y2 + move2[1] * step;
+            } else {
+                nx2 = ex2;
+                ny2 = ey2;
+            }
+            
+            // 检查当前步是否相交，包括交叉移动的情况
+            if(nx1 == nx2 && ny1 == ny2) return false;
+            
+            // 检查是否存在交叉移动
+            if(step > 1 && 
+               nx1 == prevX2 && ny1 == prevY2 && 
+               nx2 == prevX1 && ny2 == prevY1) {
+                return false;
+            }
+            
+            prevX1 = nx1; prevY1 = ny1;
+            prevX2 = nx2; prevY2 = ny2;
         }
         return true;
     }
+    
+    private int prevX1, prevY1, prevX2, prevY2; // 添加为类成员变量
 
     private boolean checkTwo(int i, int j, int[] move1, int[] move2){
-        int x1 = positions[i][0] - 1, y1 = positions[i][1], x2 = positions[j][0], y2 = positions[j][1];
+        // int x1 = positions[i][0] - 1, y1 = positions[i][1] - 1, x2 = positions[j][0] - 1, y2 = positions[j][1] - 1;
+        // for(int step = 1;step <= Math.max(move1[2], move2[2]);step++){
+        //     int nx1 = x1, nx2 = x2, ny1 = y1, ny2 = y2;
+        //     if(step <= move1[2]){
+        //         nx1 = x1 + move1[0];
+        //         ny1 = y1 + move1[1];
+        //     }
+        //     if(step <= move2[2]){
+        //         nx2 = x2 + move2[0];
+        //         ny2 = y2 + move2[1];
+        //     }
+        //     if(nx1 < 0 || nx1 >= 8 || ny1 < 0 || ny1 >= 8 || nx2 < 0 || ny2 >= 8 || ny2 < 0|| ny2 >= 8 || (nx1 == nx2 && ny1 == ny2)) {
+        //         // System.out.println("false");
+        //         return false;
+        //     }
+        //     // System.out.println("nx1 = " + nx1 + " nx2 = " + nx2 + " ny1 = " + ny1 + " ny2 = " + ny2);
+        //     x1 = nx1;
+        //     x2 = nx2;
+        //     y1 = ny1;
+        //     y2 = ny2;
+        // }
+        // return true;
+
+        // int x1 = positions[i][0] - 1, y1 = positions[i][1] - 1;
+        // int x2 = positions[j][0] - 1, y2 = positions[j][1] - 1;
+        
+        // for(int step = 1;step <= Math.max(move1[2], move2[2]);step++){
+        //     int nx1 = x1, nx2 = x2, ny1 = y1, ny2 = y2;
+        //     if(step <= move1[2]){
+        //         nx1 = x1 + move1[0] * step;
+        //         ny1 = y1 + move1[1] * step;
+        //     }
+        //     if(step <= move2[2]){
+        //         nx2 = x2 + move2[0] * step;
+        //         ny2 = y2 + move2[1] * step;
+        //     }
+        //     if(nx1 < 0 || nx1 >= 8 || ny1 < 0 || ny1 >= 8 || 
+        //     nx2 < 0 || nx2 >= 8 || ny2 < 0 || ny2 >= 8 || 
+        //     (nx1 == nx2 && ny1 == ny2)) {
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        int x1 = positions[i][0] - 1, y1 = positions[i][1] - 1;
+        int x2 = positions[j][0] - 1, y2 = positions[j][1] - 1;
+        
         for(int step = 1;step <= Math.max(move1[2], move2[2]);step++){
             int nx1 = x1, nx2 = x2, ny1 = y1, ny2 = y2;
+            // 计算第一个棋子的位置
             if(step <= move1[2]){
-                nx1 = x1 + move1[0];
-                ny1 = y1 + move1[1];
+                nx1 = x1 + move1[0] * step;
+                ny1 = y1 + move1[1] * step;
+            }else{
+                nx1 = x1 + move1[0] * move1[2];
+                ny1 = y1 + move1[1] * move1[2];
             }
+            
+            // 计算第二个棋子的位置
             if(step <= move2[2]){
-                nx2 = x2 + move2[0];
-                ny2 = y2 + move2[1];
+                nx2 = x2 + move2[0] * step;
+                ny2 = y2 + move2[1] * step;
+            }else{
+                nx2 = x2 + move2[0] * move2[2];
+                ny2 = y2 + move2[1] * move2[2];
             }
-            if(nx1 < 0 || nx1 >= 8 || ny1 < 0 || ny1 >= 8 || nx2 < 0 || ny2 >= 8 || ny2 < 0|| ny2 >= 8 || (nx1 == nx2 && ny1 == ny2)) return false;
-            x1 = nx1;
-            x2 = nx1;
-            y1 = ny1;
-            y2 = ny2;
+            
+            // 检查边界和碰撞
+            if(nx1 < 0 || nx1 >= 8 || ny1 < 0 || ny1 >= 8 || 
+            nx2 < 0 || nx2 >= 8 || ny2 < 0 || ny2 >= 8 || 
+            (nx1 == nx2 && ny1 == ny2)) {
+                return false;
+            }
         }
         return true;
     }
@@ -129,7 +254,7 @@ public class LC2056 {
         int[][] positions = new int[][]{{1, 1}};
         System.out.println(new LC2056().countCombinations(pieces, positions));
 
-        // System.out.println("-----------------------------");
+        System.out.println("-----------------------------");
 
         // // 22
         // pieces = new String[]{"queen"};
@@ -146,11 +271,16 @@ public class LC2056 {
         // positions = new int[][]{{1, 1}, {8, 8}};
         // System.out.println(new LC2056().countCombinations(pieces, positions));
 
-        System.out.println("-----------------------------");
+        // // System.out.println("-----------------------------");
 
-        // 281
-        pieces = new String[]{"queen","bishop"};
-        positions = new int[][]{{5,7}, {3,4}};
+        // // 281
+        // pieces = new String[]{"queen","bishop"};
+        // positions = new int[][]{{5,7}, {3,4}};
+        // System.out.println(new LC2056().countCombinations(pieces, positions));
+
+        // 293
+        pieces = new String[]{"queen","rook"};
+        positions = new int[][]{{1, 2}, {7, 2}};
         System.out.println(new LC2056().countCombinations(pieces, positions));
     }
 }
