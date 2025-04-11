6e8f20c57066192e44a5b64d8094366b7fc3c4cc
SnowWolf68 <130046153+SnowWolf68@users.noreply.github.com>
2025-04-11 13:13:25 +0800

    update

--- revise_problemList/LC3376_MLE_1.java
+++ revise_problemList/LC3376_MLE_1.java
@@ -7,8 +7,10 @@ import java.util.List;
  * 最朴素的想法
  * 每次(每分钟)的选择: 1)什么都不做 2)开第i把锁
  * dfs
+ * 
+ * 这种写法的curStrength的上限是 sum(strength) , 这个上限会比较高, 因此可能会爆内存
  */
-public class LC3376_MLE {
+public class LC3376_MLE_1 {
     private int sum = 0, INF = 0x3f3f3f3f;
     private int[][][] memo;
     public int findMinimumTime(List<Integer> strength, int k) {
@@ -35,7 +37,13 @@ public class LC3376_MLE {
         ret = dfs(strength, k, mask, state, x, curStrength + x) + 1;
         // 尝试开第i把锁
         for(int i = 0;i < n;i++){
-            if(curStrength >= strength.get(i)){
+            if(curStrength >= strength.get(i) && ((state >> i) & 1) == 0){
+                /*
+                 * 为什么这里不用 dfs(...) + 1 ?
+                 * 因为从示例1可以看到, 如果在某时刻选择开锁, 那么到下一分钟时, 还是可以继续累加剑的能量的
+                 * 因此可以看做是: 开锁不需要花费时间
+                 * 故这里不需要 + 1
+                 */
                 ret = Math.min(ret, dfs(strength, k, mask, state | (1 << i), x + k, 0));
             }
         }
--- revise_problemList/LC3376_MLE_2.java
+++ revise_problemList/LC3376_MLE_2.java
@@ -4,23 +4,18 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * 
- * 
- * dp[state][x][curStrength]:
- *      1. dp[state][x][curStrength - 1] + 1
- *      2. dp[state | (1 << i)][x + k][0] +
- * 
+ * 改进: 可以强制规定: 如果curStrength到达了还未开锁的锁的能量的最小值, 那么强制开这把锁
+ * 因此curStrength的上限可以被减小到 max(strength)
  */
-public class LC3376_WA {
-    private int sum = 0, INF = 0x3f3f3f3f;
+public class LC3376_MLE_2 {
+    private int INF = 0x3f3f3f3f, maxStrength = 0;
     private int[][][] memo;
     public int findMinimumTime(List<Integer> strength, int k) {
-        int n = strength.size(), mask = (1 << n) - 1, maxStrength = 0;
+        int n = strength.size(), mask = (1 << n) - 1;
         for(int s : strength) {
-            sum += s;
             maxStrength = Math.max(maxStrength, s);
         }
-        memo = new int[mask + 1][85][sum + 1];
+        memo = new int[mask + 1][85][2 * maxStrength + 1];
         for(int i = 0;i <= mask;i++){
             for(int j = 0;j < 85;j++){
                 Arrays.fill(memo[i][j], -1);
@@ -31,15 +26,15 @@ public class LC3376_WA {
 
     private int dfs(List<Integer> strength, int k, int mask, int state, int x, int curStrength){
         if(state == mask) return 0;
-        if(curStrength > sum) return INF;
+        if(curStrength > 2 * maxStrength) return INF;
         if(memo[state][x][curStrength] != -1) return memo[state][x][curStrength];
         int ret = INF, n = strength.size();
         // 什么都不做
         ret = dfs(strength, k, mask, state, x, curStrength + x) + 1;
         // 尝试开第i把锁
         for(int i = 0;i < n;i++){
-            if(curStrength >= strength.get(i)){
-                ret = Math.min(ret, dfs(strength, k, mask, state | (1 << i), x + k, 0) + 1);
+            if(curStrength >= strength.get(i) && ((state >> i) & 1) == 0){
+                ret = Math.min(ret, dfs(strength, k, mask, state | (1 << i), x + k, 0));
             }
         }
         memo[state][x][curStrength] = ret;
