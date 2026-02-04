43fb99dcd971af27e2a760d3a430e6efcdb9e016
SnowWolf68 <130046153+SnowWolf68@users.noreply.github.com>
2026-02-04 13:53:11 +0800

    add C/C++ configurations

---
+++ SolutionsWithCorCpp/test_cpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int aa = 100;
+    
+    cout << "hello cc" << endl;
+    int a;
+    cin >> a;
+    cout << "a = " << a << endl;
+
+    return 0;
+}
+
+
